bc9cc3a0d79a82e11f4f6fe2a305d91f439d2bde
Ilya <77529856+NamianDev@users.noreply.github.com>
2021-09-15 23:00:08 +0700

    Add files via upload

---
+++ Scripts/CameraRotateAround.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CameraRotateAround : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public float sensitivity = 3; // чувствительность мышки
+    public float limit = 80; // ограничение вращения по Y
+    public float zoom = 0.25f; // чувствительность при увеличении, колесиком мышки
+    public float zoomMax = 10; // макс. увеличение
+    public float zoomMin = 3; // мин. увеличение
+    public bool FreeMode;
+    public Toggle[] m_Toggle;
+    private float X;
+
+    bool IsDestroy;
+    public MeshFilter go;
+
+    Vector3 EndPosition;
+
+    void Start()
+    {
+        limit = Mathf.Abs(limit);
+        if (limit > 90) limit = 90;
+        offset = new Vector3(offset.x, offset.y, -Mathf.Abs(zoomMax) / 2);
+        transform.position = target.position + offset;
+    }
+
+    void Update()
+    {
+        if (FreeMode)
+        {
+            offset.z = Mathf.Clamp(offset.z, -Mathf.Abs(zoomMax), -Mathf.Abs(zoomMin));
+
+            X = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+            transform.localEulerAngles = new Vector3(0, X, 0);
+            transform.position = transform.localRotation * offset + target.position;  //Камера смотрит на таргет
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 1000))
+                {
+                    //Не реализованно
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    EndPosition = hit.point;
+                    if (m_Toggle[0].isOn)
+                    {
+                       //Не реализованно
+                    }
+                    if (m_Toggle[2].isOn)
+                    {
+                        if (hit.rigidbody != null)
+                        {
+                            hit.rigidbody.velocity = transform.forward * 40;
+                        }
+                    } //Destroy
+                    else if (m_Toggle[1].isOn && !IsDestroy)
+                    {
+                        target.GetComponent<DivideCube>().DivideIntoCuboids();
+                        IsDestroy = true;
+                    }//Brick
+                    Debug.Log(EndPosition);
+                }
+            }
+        }
+    }
+
+    public void SetFreeModeBoolButton()
+    {
+        FreeMode = !FreeMode;
+    } //Кнопка свободного перемещения камеры
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(0);
+    } //Перезапуск сцены
+
+
+}
---
+++ Scripts/DivideCube.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DivideCube : MonoBehaviour
+{
+    public GameObject TargetCube;
+    Vector3 SelectionCount;
+    public Material SubMaterial;
+    public CameraRotateAround cameraRotateAround;
+    public Scrollbar[] AllScrollBars;
+
+    private Vector3 SizeOfOriginalCube, SelectionSize, FillStartPos;
+    Transform ParentTransform;
+    GameObject SubCube;
+
+    void Start()
+    {
+        if (TargetCube == null)
+        {
+            TargetCube = gameObject;
+        }
+        SizeOfOriginalCube = TargetCube.transform.lossyScale;
+
+        FillStartPos = TargetCube.transform.TransformPoint(new Vector3(-0.5f, -0.3f, -0.5f))
+            + TargetCube.transform.TransformDirection(new Vector3(SelectionSize.x, -SelectionSize.y, SelectionSize.z) / 2); 
+        ParentTransform = new GameObject(TargetCube.name + "CubeParent").transform; //Создание пустого родителя
+        ParentTransform.position = TargetCube.transform.position;
+    }
+    public void DivideIntoCuboids()
+    {
+        SelectionCount = new Vector3(
+            ((float)System.Math.Round(AllScrollBars[0].value, 1) * 10),
+            ((float)System.Math.Round(AllScrollBars[1].value, 1) * 10),
+            ((float)System.Math.Round(AllScrollBars[2].value, 1) * 10)
+        ); //Считаем размер сетки из ScrollBar
+        SelectionSize = new Vector3(
+            SizeOfOriginalCube.x / SelectionCount.x,
+            SizeOfOriginalCube.y / SelectionCount.y,
+            SizeOfOriginalCube.z / SelectionCount.z
+            ); //Размер дочерних кубов
+        int SellCount = 0;//Кол-во дочерних кубов
+        for (int x = 0; x < SelectionCount.x; x++)
+        {
+            for (int y = 0; y < SelectionCount.y; y++)
+            {
+                for (int z = 0; z < SelectionCount.z; z++)
+                {
+                    SubCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                    SubCube.transform.localScale = SelectionSize;
+                    SubCube.transform.position = FillStartPos + TargetCube.transform.TransformDirection(new Vector3((SelectionSize.x) * x, (SelectionSize.y) * y, (SelectionSize.z) * z));
+                    SubCube.transform.rotation = TargetCube.transform.rotation;
+
+                    SubCube.transform.SetParent(ParentTransform);
+                    SubCube.GetComponent<MeshRenderer>().material = SubMaterial;
+                    SellCount++;
+                    SubCube.AddComponent<Rigidbody>();  //Добавление физики
+                }
+            }
+        }
+        Debug.Log(SellCount);
+        Destroy(TargetCube); //Уничтожить таргет для камеры и назначить новый
+        cameraRotateAround.target = ParentTransform;
+    }
+}
